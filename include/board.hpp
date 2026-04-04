f57e789c6b824f81783029798bebb8201029f7aa
max <hudma@utschools.ca>
2026-04-03 23:42:29 -0400

    no change

--- include/board.hpp
+++ include/board.hpp
@@ -288,7 +288,7 @@ public:
         return std::string(board.begin(), board.end());
     }
     bool isFull() const {
-        return std::all_of(board.begin(), board.end(), [](char c) { return c != EMPTY_CHAR; });
+        return availableToPlay.size() == BoardUtils::ipow(this->size, this->dimensions);
     }
     bool isEmpty() const {
         return std::all_of(board.begin(), board.end(), [](char c) { return c == EMPTY_CHAR; });
